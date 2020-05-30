0672060612fe7a9c20371d30bb856c3256de5c53
Ramiro Vargas <ramiro.vargas@masivian.com>
2020-05-30 12:49:04 -0500

    add bottom menu

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:prohumanhacks/screens/home.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return HomePage();
-  }
-}
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(fontFamily: 'Roboto', hintColor: Color(0xFFd0cece)),
+    home: Text("hello world"),
+));
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prohumanhacks/widgets/BottomNavBarWidget.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,7 +10,38 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Home page");
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFFFAFAFA),
+          elevation: 0,
+          title: Text(
+            "What would you like to eat?",
+            style: TextStyle(
+                color: Color(0xFF3a3737),
+                fontSize: 16,
+                fontWeight: FontWeight.w500),
+          ),
+          brightness: Brightness.light,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+                  Icons.notifications_none,
+                  color: Color(0xFF3a3737),
+                ))
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+// Too add widgets
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavBarWidget(),
+      );
+    }
 
   }
 }
